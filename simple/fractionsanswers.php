2521a0e16ba8184315c7f7297965a2a684c83f67
Yang Yang <32833506+yaya7264@users.noreply.github.com>
2017-12-06 16:55:05 -0700

    Add files via upload

---
+++ simple/fractionsanswers.php
@@ -0,0 +1,18 @@
+<html>
+
+	<head>
+		<title>Information Gathered</title>
+	</head>
+
+
+	<body>
+		<?php
+		$usersName = $_POST['username'];
+		$streetAddress = $_POST['streetaddress'];
+		$cityAddress = $_POST['cityaddress'];
+		echo '<p>Your Information</p>';
+		echo $usersName. ' lives at </br>';
+		 ?>
+	</body>
+
+</html>
